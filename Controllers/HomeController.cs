e0aae5981f8f3d13c5cc638cc94e2d6dd317e549
gtalluri123 <gtalluri@gmail.com>
2017-07-18 06:46:07 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PropRecon.Biz;
+using Newtonsoft.Json;
+using PropRecon.Biz.entities;
+namespace PropRecon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            new ProcessFindPersonRequest().ProcessRequest();
+            return View();
+
+        }
+        public JsonResult GetData()
+        {
+            var context = new UnClaimedEntities();
+            //var results = context.TexasRecords.SqlQuery("select * from uptexas where AmountRemitted >@p0 order by AmountRemitted desc", 10000);
+            //var results = context.TexasRecords.Where(r=>r.AmountRemitted>10000).OrderByDescending(r=>r.AmountRemitted);
+            var results = context.Database.SqlQuery <EnrichedPerson>("exec SearchPeople @amount,@lastname ", 10000,"Talluri");
+            //var results = context.TexasRecords.Join(context.People,r=>r.FormerOwnerFirst,p=>p.FirstName,
+              
+            return Json(results, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PropRecon.WebApiApplication" Language="C#" %>
